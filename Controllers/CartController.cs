e7200e848d38edcab45d7dc0c07b5f1cd5b9c82d
Mariam Alaa <mariam.alaa72005@gmail.com>
2026-08-12 17:23:44 +0300

    shoping cart added

---
+++ Controllers/CartController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Online_Restaurant.Controllers
+{
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
